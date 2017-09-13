53eb2fe677325fa88013c7134d46b465fec1eb3d
Sai Chandramouli <schand31@asu.edu>
2017-09-13 12:02:21 -0700

    added dragula base, sass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dragula from 'react-dragula';
 
 class App extends Component {
+  dragulaDecorator = (componentBackingInstance) => {
+    if (componentBackingInstance) {
+      let options = { };
+      Dragula([componentBackingInstance], options);
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React Drag Drop</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container' ref={this.dragulaDecorator}>
+          <div>Swap me around</div>
+          <div>Swap her around</div>
+          <div>Swap him around</div>
+          <div>Swap them around</div>
+          <div>Swap us around</div>
+          <div>Swap things around</div>
+          <div>Swap everything around</div>
+        </div>
       </div>
     );
   }
